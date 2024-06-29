996b63dc3491c63ba4f181c4afacc967242ac797
Atish Jain <atish_jain@yahoo.com>
2024-06-29 11:00:20 +0530

    Add files via upload

---
+++ 008_chardemo.c
@@ -0,0 +1,13 @@
+//Program to read a single character and display the same
+//program name: 008_chardemo.c
+
+# include <stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter a character...: ");
+    scanf("%c",&ch);
+    printf("The Character is: %c",ch);
+
+    return 0;
+}
---
+++ 010_readfullname.c
@@ -0,0 +1,14 @@
+//Program to read a name and display the same
+//program name: 010_readfullname.c
+
+# include <stdio.h>
+int main()
+{
+    char name[100];
+    printf("Heyy Enter ur name...:");
+    scanf("%[^\n]",&name);
+
+    printf("Hello %s, Welcome to C Language classes",name);
+
+    return 0;
+}
